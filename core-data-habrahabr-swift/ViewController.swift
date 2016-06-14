1a26dc808bbfcbe52bdec4cb4f6f8f31815354d3
Antony <antony.karpov@icloud.com>
2016-06-14 11:58:01 +0300

    Initial Commit

---
+++ core-data-habrahabr-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  core-data-habrahabr-swift
+//
+//  Created by Anton Karpov on 14.06.16.
+//  Copyright © 2016 Antony Karpov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
